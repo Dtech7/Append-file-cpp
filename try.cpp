68382ef646ed7ede354e377b10616744faf86eef
Neil D Felix <neild.felix@gmail.com>
2017-12-07 21:13:41 -0500

    Update try.cpp

--- try.cpp
+++ try.cpp
@@ -1,48 +1,77 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
    string ln;
+   string inln;
    ifstream mfl ("exp.txt");
-   ofstream mflW ("exp.txt");
+   ofstream mflw ("exp.txt");
    int numln = 0;
+   vector <string> lnAr;
    
    if (mfl.is_open())
    {
 
-     while(!mfl.eof())
+     while( getline(mfl, ln) );
      {
-          getline(mfl,ln);
+          
+          cout << ln << endl;
+          inln = ln;
+          lnAr.push_back(inln);
           numln++;
-     }
-     cout << numln;
 
-     if(numln <= 2)
+     }
+     
+     
+     cout << numln +1 << endl;
+     
+     /*for (int j=0; j<lnAr.size(); j++)
      {
-       cout << "Not enough lines. " << endl;
-       cout << "Adding more lines..." << endl;
-
-       mflW << "This was added because there wasn't enough lines." << endl ;
-       mflW << "It should be good now." << endl;
-     }
-       
-     else cout << "Good!" << endl;
+           cout << lnAr[j] << endl;
+     }*/
+     
+     mfl.close();
+    }
+    else cout << "Unable to open file";
 
 
-     while( getline (mfl, ln))
+    if(mflw.is_open())
+    { 
+      if(numln <= 2)
+      {
+        cout << "Not enough lines. " << endl;
+        cout << "Adding more lines..." << endl;
+
+        lnAr.push_back("This was added because there wasn't enough lines.");
+        lnAr.push_back("It should be good now.");
+        
+        for (int i=0; i<lnAr.size(); i++)
+        {
+           mflw << lnAr[i] << endl;
+        
+        
+        }
+        
+       }
+       mflw.close();
+     } 
+     else cout << "Unable to open file";  
+    
+
+
+     /*while( getline (mfl, ln))
      {
         cout << ln << '\n';
 
-     }
+     }*/
 
-     mfl.close();
-
-   }
    
-   else cout << "Unable to open file";
+   
+   
 
 
 
